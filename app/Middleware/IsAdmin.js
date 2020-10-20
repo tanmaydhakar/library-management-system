669c07ae0707e8088cfd585d79b1f2d26b61d781
tanmay <tanmay@gkmits-MacBook-Air.local>
2020-10-20 21:10:01 +0530

    auth setup in progress

---
+++ app/Middleware/IsAdmin.js
@@ -0,0 +1,25 @@
+'use strict'
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+class IsAdmin {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+  async handle ({ request, response }, next) {
+    
+    // const user = request.user;
+
+    // if(user.role != 'Admin'){
+    //   return response.status(403).json("Unauthorized");
+    // }else{
+    // }
+    await next();
+
+  }
+}
+
+module.exports = IsAdmin
